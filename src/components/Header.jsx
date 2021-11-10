ae79a538902ba31a4adc66ed7d04aff28e654a4c
Leandro Nogueira <leandro2903.lan@gmail.com>
2021-11-10 18:48:20 -0300

    criando um header com a lista de futuros links

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+// import { Link } from "react-router-dom";
+import '../style/Header.css';
+
+export default function Header() {
+  return (
+    <header>
+      <img src="#" alt="Logo" />
+      <nav>
+        <ul>
+          <li>Sobre</li>
+          <li>Serviços</li>
+          <li>Blog</li>
+          <li>Contatos</li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
